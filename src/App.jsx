88381a6239a756a242aa3d9b5578a0c5aae4d61e
Hitesh Kanwal <singhhitesh442@gmail.com>
2024-09-17 14:43:43 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Navbar from './components/navbar'
+import Manager from './components/manager'
+import Footer from './components/footer'
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+    <div className="[background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]">"
+        <Manager />
+      </div>
+      <Footer />
+    </>
+  )
+}
+
+export default App
